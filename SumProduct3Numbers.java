8cbbaec92f6751c6cb92a51e46323afd47b8acfd
Anurag Anand <anuraganand448@gmail.com>
2023-01-12 15:51:18 +0530

    Add files via upload

---
+++ SumProduct3Numbers.java
@@ -0,0 +1,10 @@
+public class SumProduct3Numbers{
+public static void main(String[] args){
+int n1=98,n2=5,n3=77;
+System.out.println("Sum of 98, 5, and 77 is ");
+System.out.println(n1+n2+n3);
+System.out.println("product of 98, 5 ,and 77 is ");
+System.out.println(n1*n2*n3);
+
+
+}}
